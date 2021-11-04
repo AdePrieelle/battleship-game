2f7a82e2601f4a5401ad86d9d5c3dc554726b9b5
Antoine de Prieelle <antoine_@live.nl>
2021-11-04 13:54:41 +0100

    Refactor function createRandomGameboard and add jest test

--- src/features/game/createRandomGameboard/__tests__/createRandomGameboard.test.js
+++ src/features/game/createRandomGameboard/__tests__/createRandomGameboard.test.js
@@ -1,6 +1,131 @@
 import { createRandomGameboard } from "../createRandomGameboard";
 import { ships } from '../../ships';
 
-test("create a random gameboard with ships placed", () => {
-  expect(createRandomGameboard(10, 10, "empty", ships)).toBe(true);
+test("create a random gameboard with randomly generated ships coords placed correctly", () => {
+  const mockGenerateRandomValidShipPosition = jest
+    .fn()
+    .mockReturnValueOnce(
+      {
+        startIndex: 0,
+        direction: "horizontal",
+        shipCoords: [0, 1, 2, 3]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 5,
+        direction: "horizontal",
+        shipCoords: [5, 6, 7]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 9,
+        direction: "horizontal",
+        shipCoords: [9, 19, 29]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 20,
+        direction: "vertical",
+        shipCoords: [20, 30]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 24,
+        direction: "vertical",
+        shipCoords: [24, 34]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 60,
+        direction: "horizontal",
+        shipCoords: [60, 61]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 80,
+        direction: "horizontal",
+        shipCoords: [80]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 83,
+        direction: "vertical",
+        shipCoords: [83]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 85,
+        direction: "vertical",
+        shipCoords: [85]
+      }
+    )
+    .mockReturnValueOnce(
+      {
+        startIndex: 98,
+        direction: "vertical",
+        shipCoords: [98]
+      }
+    );
+  const mockShips = jest.fn(() => ([
+    {
+      name: "d1",
+      shipLength: 1,
+    },
+    {
+      name: "d2",
+      shipLength: 1,
+    },
+    {
+      name: "d3",
+      shipLength: 1,
+    },
+    {
+      name: "d4",
+      shipLength: 1,
+    },
+    {
+      name: "s1",
+      shipLength: 2,
+    },
+    {
+      name: "s2",
+      shipLength: 2,
+    },
+    {
+      name: "s3",
+      shipLength: 2,
+    },
+    {
+      name: "b1",
+      shipLength: 3,
+    },
+    {
+      name: "b2",
+      shipLength: 3,
+    },
+    {
+      name: "c1",
+      shipLength: 4,
+    },
+  ]));
+  expect(createRandomGameboard(10, 10, "empty", mockGenerateRandomValidShipPosition, mockShips())).toStrictEqual([
+    "c1", "c1", "c1", "c1", "empty", "b1", "b1", "b1", "empty", "b2",
+    "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "b2",
+    "s1", "empty", "empty", "empty", "s2", "empty", "empty", "empty", "empty", "b2",
+    "s1", "empty", "empty", "empty", "s2", "empty", "empty", "empty", "empty", "empty",
+    "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty",
+    "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty",
+    "s3", "s3", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty",
+    "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty",
+    "d1", "empty", "empty", "d2", "empty", "d3", "empty", "empty", "empty", "empty",
+    "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "d4", "empty",
+  ]);
 });
--- src/features/game/createRandomGameboard/createRandomGameboard.js
+++ src/features/game/createRandomGameboard/createRandomGameboard.js
@@ -9,21 +9,14 @@ import { getFirstDigitOfNumber } from '../getFirstDigitOfNumber/getFirstDigitOfN
 import { generateRandomShipPosition } from '../generateRandomShipPosition/generateRandomShipPosition';
 import { isValidShipPosition } from '../isValidShipPosition/isValidShipPosition';
 import { isEmptyGameboardCell } from '../isEmptyGameboardCell/isEmptyGameboardCell';
-import { generateRandomValidShipPosition } from '../generateRandomValidShipPosition/generateRandomValidShipPosition';
 
-export const createRandomGameboard = (amountRows, amountColumns, emptyGameboardValue, ships) => {
-  
+export const createRandomGameboard = (amountRows, amountColumns, emptyGameboardValue, generateRandomValidShipPosition, ships) => {
   const randomGameboard = createGameboard(amountRows, amountColumns, emptyGameboardValue);
   const horizontalDirectionValue = "horizontal";
   const verticalDirectionValue = "vertical";
-
   const sortedShipsLengthDescendingOrder = sortArrayOfObjectsBasedOnAPropertyValue(ships, "shipLength");
-  // console.log("render createRandomGameboard");
 
-  // start this part again if timesTriedToPlaceShip more than max times to try to place a ship
-  // outer: for (const ship of sortedShipsLengthDescendingOrder) {
   for (const ship of sortedShipsLengthDescendingOrder) {
-
     const randomValidShipPosition = generateRandomValidShipPosition(
       generateRandomShipPosition,
       randomGameboard, 
@@ -43,42 +36,9 @@ export const createRandomGameboard = (amountRows, amountColumns, emptyGameboardV
       ships
     );
     
-    
-    // let randomShipPosition = generateRandomShipPosition(randomGameboard, ship, horizontalDirectionValue, verticalDirectionValue, getRandomIndexFromArray, getRandomArrayValue, calculateShipCoords);
-
-    // let timesTriedToPlaceShip = 1;
-    // // console.log(`render ${ship.name}`);
-
-    // while (!isValidShipPosition(
-    //   isEmptyGameboardCell(randomGameboard, randomShipPosition.startIndex, emptyGameboardValue),
-    //   checkIfStartIndexShipCoordsDirectionIsNotOutOfBounds(randomShipPosition.startIndex, ship, randomShipPosition.direction, horizontalDirectionValue, verticalDirectionValue, getFirstDigitOfNumber),
-    //   checkIfShipIsNotSurroundedByAnotherShip(randomGameboard, randomShipPosition.shipCoords, emptyGameboardValue) 
-    // )) {
-    //   if (timesTriedToPlaceShip >= 25) {
-    //     // console.log(`restarting createRandomGameboard, timesTriedToPlaceShip ${ship.name}: ${timesTriedToPlaceShip}`);
-    //     return createRandomGameboard(10, 10, emptyGameboardValue, ships);
-    //     // break outer;
-    //   } else {
-    //     randomShipPosition = generateRandomShipPosition(randomGameboard, ship, horizontalDirectionValue, verticalDirectionValue, getRandomIndexFromArray, getRandomArrayValue, calculateShipCoords);
-    //     timesTriedToPlaceShip += 1;
-    //   }
-      
-    // }
-    // console.log(timesTriedToPlaceShip);
-
-    
-    // for (const coord of randomShipPosition.shipCoords) {
-    //   randomGameboard[coord] = ship.name;
-    // }
-
     for (const coord of randomValidShipPosition.shipCoords) {
       randomGameboard[coord] = ship.name;
     }
-
   }
-
-  // console.table(randomGameboard);
-  // console.log(randomGameboard);
-
-  return true;
+  return randomGameboard;
 }
