bfcc9dead4da764247426b55b04ddd697065a19a
Antoine de Prieelle <antoine_@live.nl>
2022-02-02 14:22:53 +0100

    Remove unused react components and functions with their jest tests

--- src/features/game/deleteArrayIndexValue/deleteArrayIndexValue.js
+++
@@ -1,5 +0,0 @@
-export const deleteArrayIndexValue = (array, index) => {
-  let copyArray = [...array];
-  copyArray.splice(index, 1);
-  return copyArray;
-}
--- src/features/game/increaseArrayValuesWithNumber/increaseArrayValuesWithNumber.js
+++
@@ -1,4 +0,0 @@
-export const increaseArrayValuesWithNumber = (array, number) => {
-  const increasedArrayValuesWithNumber = array.map(value => value + number);
-  return increasedArrayValuesWithNumber;
-}
