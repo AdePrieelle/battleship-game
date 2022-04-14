4e9f7f7b8b820461e111704bf819dcab92c3b5b1
Antoine de Prieelle <antoine_@live.nl>
2022-04-14 15:41:25 +0200

    Update hover color during ship placement

--- src/common/utils/sortArrayOfObjectsBasedOnTwoKeyValuesByOrder/sortArrayOfObjectsBasedOnTwoKeyValuesByOrder.js
+++ src/common/utils/sortArrayOfObjectsBasedOnTwoKeyValuesByOrder/sortArrayOfObjectsBasedOnTwoKeyValuesByOrder.js
@@ -37,20 +37,3 @@ export const sortArrayOfObjectsBasedOnTwoKeyValuesByOrder = (array, keyOne, keyT
   
   return sortedArrayOfObjectsBasedOnTwoKeyValuesByOrder;
 };
-
-// export const sortArrayOfObjectsBasedOnTwoPropertyValues = (array, propertyOneToSort, propertyTwoToSort) => {
-//   let copyArray = [...array];
-//   const sortedArrayOfObjectsBasedOnAPropertyValue = copyArray.sort((one, other) => {
-//     if (other[propertyOneToSort] - one[propertyOneToSort] > 0) {
-//       return 1;
-//     }
-//     if (other[propertyOneToSort] - one[propertyOneToSort] < 0) {
-//       return -1;
-//     }
-//     else if (other[propertyOneToSort] === one[propertyOneToSort]) {
-//       return (other[propertyTwoToSort] - one[propertyTwoToSort]);
-//     }
-//     else return 0;
-//   });
-//   return sortedArrayOfObjectsBasedOnAPropertyValue;
-// };
