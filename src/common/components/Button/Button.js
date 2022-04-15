aa776be3c6f2cbdf2583d15cc935020acb328492
Antoine de Prieelle <antoine_@live.nl>
2022-04-15 15:03:29 +0200

    Add scss styling and add disabled tag to react component Button

--- src/common/components/Button/Button.js
+++ src/common/components/Button/Button.js
@@ -4,6 +4,7 @@ export const Button = ({ buttonOnClick, children, disableButton=false }) => {
   return (
     <button 
       className={`${disableButton ? "button button-disabled" : "button"}`}
+      disabled={disableButton}
       onClick={disableButton ? null : (() => buttonOnClick())}
     >
       {children}
